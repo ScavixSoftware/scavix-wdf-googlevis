5773b1733cc4fa28f2d6493e4a7f60c36c9381cf
Daniel Spors <daniels@scavix.com>
2026-02-24 08:06:23 +0100

    Performance: Use 'substr_from' instead of 'array_last(explode())'

--- lib/googlevisualization.class.php
+++ lib/googlevisualization.class.php
@@ -218,7 +218,7 @@ abstract class GoogleVisualization extends GoogleControl implements ICallable
 		require_once('MC/Google/Visualization.php');
 		return new MC_Google_Visualization(
 				new PDO($ds->GetDsn(),$ds->Username(),$ds->Password() ),
-				strtolower(array_last(explode("\\",get_class($ds->Driver))))
+				strtolower(substr_from(get_class($ds->Driver), "\\"))
 			);
 	}
 
